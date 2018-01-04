e5f4466d9367c9cb2a4fe3123ee2436608b55065
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-04 18:26:53 -0500

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("$")
+var CoinMarketCap = require("node-coinmarketcap");
+var coinmarketcap = new CoinMarketCap();
+
+bot.on('ready', function() {
+    bot.user.setGame("$pBTC");
+    console.log("Connected");
+});
+
+bot.login("MzkzMjUwNTIwODk2MTEwNTkz.DSBV6Q.GzkLp-7GQDd8_stMma-X8NAfjSo");
+
+bot.on('message', message => {
+    if (message.content === prefix + "help") {
+        message.channel.sendMessage("Liste des commandes: \n -$help \n -$BTC-USD");
+    }
+
+    
+    if (message.content === prefix + "pBTC") {
+        coinmarketcap.multi(coins => {
+            var maintenant=new Date();
+            var jour=maintenant.getDate();
+            var mois=maintenant.getMonth()+1;
+            var an=maintenant.getFullYear();
+            var heure=maintenant.getHours();
+            var minute=maintenant.getMinutes();
+            var seconde=maintenant.getSeconds();
+            var pc1 = " :chart_with_upwards_trend: ";
+            var pc2 = " :chart_with_upwards_trend: ";
+            var pc3 = " :chart_with_upwards_trend: ";
+
+            if (coins.get("BTC").percent_change_1h < 0) {
+                pc1 = ":chart_with_downwards_trend:";
+            }else {pc1 = pc1};
+            
+            if (coins.get("BTC").percent_change_24h < 0) {
+                pc2 = ":chart_with_downwards_trend:";
+            }else {pc2 = pc2};
+
+            if (coins.get("BTC").percent_change_7d < 0) {
+                pc3 = ":chart_with_downwards_trend:";
+            }else {pc3 = pc3};
+
+            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("BTC").name + "**__" + "\nRank: " + coins.get("BTC").rank + "\nPrice : " + coins.get("BTC").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("BTC").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("BTC").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("BTC").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
+            console.log("Commande BTC-USD effectuée");
+        });
+    }
+});
