e4b25611d4ae4cbb4231b50255edab90d6b9dca9
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-05 01:01:19 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,7 +17,6 @@ bot.on('message', message => {
         message.channel.sendMessage("__**Liste des commandes:**__ \n -$help \n -$pBTC");
     }
 
-bot.on('message', message => {
 // BTC
     if (message.content === prefix + "pBTC") {
         coinmarketcap.multi(coins => {
@@ -48,11 +47,9 @@ bot.on('message', message => {
             console.log("Commande BTC-USD effectuée");
         });
     }
-}
-       
-bot.on('message', message => {
-// BTC
-    if (message.content === prefix + "pETH") {
+
+// XBY
+    if (message.content === prefix + "pXBY") {
         coinmarketcap.multi(coins => {
             var maintenant=new Date();
             var jour=maintenant.getDate();
@@ -65,22 +62,27 @@ bot.on('message', message => {
             var pc2 = " :chart_with_upwards_trend: ";
             var pc3 = " :chart_with_upwards_trend: ";
 
-            if (coins.get("BTC").percent_change_1h < 0) {
+            if (coins.get("XBY").percent_change_1h < 0) {
                 pc1 = ":chart_with_downwards_trend:";
             }else {pc1 = pc1};
             
-            if (coins.get("BTC").percent_change_24h < 0) {
+            if (coins.get("XBY").percent_change_24h < 0) {
                 pc2 = ":chart_with_downwards_trend:";
             }else {pc2 = pc2};
 
-            if (coins.get("BTC").percent_change_7d < 0) {
+            if (coins.get("XBY").percent_change_7d < 0) {
                 pc3 = ":chart_with_downwards_trend:";
             }else {pc3 = pc3};
 
-            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("BTC").name + "**__" + "\nRank: " + coins.get("BTC").rank + "\nPrice : " + coins.get("BTC").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("BTC").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("BTC").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("BTC").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
-            console.log("Commande BTC-USD effectuée");
+            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("XBY").name + "**__" + "\nRank: " + coins.get("XBY").rank + "\nPrice : " + coins.get("XBY").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("XBY").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("XBY").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("XBY").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
+            console.log("Commande $pXBY effectuée");
         });
     }
-}
-//end
+    
+    if (message.content === "$loop") { 
+      var interval = setInterval (function () {
+        message.channel.send("123")
+      }, 300000); 
+    }
+    
 });
