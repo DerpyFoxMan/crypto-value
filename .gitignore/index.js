10c89f5bc203e2bebdad0e16d5df8ace12863d71
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-05 00:36:32 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -47,4 +47,36 @@ bot.on('message', message => {
             console.log("Commande BTC-USD effectuée");
         });
     }
+    
+    // test
+    if (message.content === prefix + "pXBY") {
+        coinmarketcap.multi(coins => {
+            var maintenant=new Date();
+            var jour=maintenant.getDate();
+            var mois=maintenant.getMonth()+1;
+            var an=maintenant.getFullYear();
+            var heure=maintenant.getHours();
+            var minute=maintenant.getMinutes();
+            var seconde=maintenant.getSeconds();
+            var pc1 = " :chart_with_upwards_trend: ";
+            var pc2 = " :chart_with_upwards_trend: ";
+            var pc3 = " :chart_with_upwards_trend: ";
+
+            if (coins.get("BTC").percent_change_1h < 0) {
+                pc1 = ":chart_with_downwards_trend:";
+            }else {pc1 = pc1};
+            
+            if (coins.get("BTC").percent_change_24h < 0) {
+                pc2 = ":chart_with_downwards_trend:";
+            }else {pc2 = pc2};
+
+            if (coins.get("BTC").percent_change_7d < 0) {
+                pc3 = ":chart_with_downwards_trend:";
+            }else {pc3 = pc3};
+
+            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("BTC").name + "**__" + "\nRank: " + coins.get("BTC").rank + "\nPrice : " + coins.get("BTC").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("BTC").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("BTC").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("BTC").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
+            console.log("Commande BTC-USD effectuée");
+        });
+    }
+    //fin test
 });
