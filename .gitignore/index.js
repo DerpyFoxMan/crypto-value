8059e0975ebcf832155a3de76eeb04a33efb4ae2
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-04 23:58:11 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -107,5 +107,5 @@ bot.on('message', message => {
         message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("XBY").name + "**__" + "\nRank: " + coins.get("XBY").rank + "\nPrice : " + coins.get("XBY").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("XBY").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("XBY").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("XBY").percent_change_7d + " %" + pc3 )
       }, 300000); 
     }
-    
+    });
 });
