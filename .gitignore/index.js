0a6dcd9f197018a301ec9e68d2bda3a67bf58c5b
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-05 00:54:34 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -49,7 +49,7 @@ bot.on('message', message => {
     }
 
 // XBY
-    if (message.content === prefix + "pXBY") {
+    if (message.content === prefix + "pETH") {
         coinmarketcap.multi(coins => {
             var maintenant=new Date();
             var jour=maintenant.getDate();
@@ -62,20 +62,20 @@ bot.on('message', message => {
             var pc2 = " :chart_with_upwards_trend: ";
             var pc3 = " :chart_with_upwards_trend: ";
 
-            if (coins.get("XBY").percent_change_1h < 0) {
+            if (coins.get("ETH").percent_change_1h < 0) {
                 pc1 = ":chart_with_downwards_trend:";
             }else {pc1 = pc1};
             
-            if (coins.get("XBY").percent_change_24h < 0) {
+            if (coins.get("ETH").percent_change_24h < 0) {
                 pc2 = ":chart_with_downwards_trend:";
             }else {pc2 = pc2};
 
-            if (coins.get("XBY").percent_change_7d < 0) {
+            if (coins.get("ETH").percent_change_7d < 0) {
                 pc3 = ":chart_with_downwards_trend:";
             }else {pc3 = pc3};
 
-            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("XBY").name + "**__" + "\nRank: " + coins.get("XBY").rank + "\nPrice : " + coins.get("XBY").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("XBY").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("XBY").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("XBY").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
-            console.log("Commande $pXBY effectuée");
+            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("ETH").name + "**__" + "\nRank: " + coins.get("ETH").rank + "\nPrice : " + coins.get("ETH").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("ETH").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("ETH").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("ETH").percent_change_7d + " %" + pc3 );// Prints price of ETH in USD
+            console.log("Commande $pETH effectuée");
         });
     }
 
