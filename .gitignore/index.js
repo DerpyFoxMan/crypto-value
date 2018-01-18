4b3fd3383b1df8ea5c2c0163dfa44e916add30b4
DerpyFoxMan <34697861+DerpyFoxMan@users.noreply.github.com>
2018-01-17 20:40:41 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -76,13 +76,36 @@ bot.on('message', message => {
 
             message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("XBY").name + "**__" + "\nRank: " + coins.get("XBY").rank + "\nPrice : " + coins.get("XBY").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("XBY").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("XBY").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("XBY").percent_change_7d + " %" + pc3 );// Prints price of BTC in USD
             console.log("Commande $pXBY effectuée");
+            
+//UBIQ
+            
+    if (message.content === prefix + "pUBIQ") {
+        coinmarketcap.multi(coins => {
+            var maintenant=new Date();
+            var jour=maintenant.getDate();
+            var mois=maintenant.getMonth()+1;
+            var an=maintenant.getFullYear();
+            var heure=maintenant.getHours();
+            var minute=maintenant.getMinutes();
+            var seconde=maintenant.getSeconds();
+            var pc1 = " :chart_with_upwards_trend: ";
+            var pc2 = " :chart_with_upwards_trend: ";
+            var pc3 = " :chart_with_upwards_trend: ";
+
+            if (coins.get("UBIQ").percent_change_1h < 0) {
+                pc1 = ":chart_with_downwards_trend:";
+            }else {pc1 = pc1};
+            
+            if (coins.get("UBIQ").percent_change_24h < 0) {
+                pc2 = ":chart_with_downwards_trend:";
+            }else {pc2 = pc2};
+
+            if (coins.get("UBIQ").percent_change_7d < 0) {
+                pc3 = ":chart_with_downwards_trend:";
+            }else {pc3 = pc3};
+
+            message.channel.sendMessage("*" + jour + "-" + mois + "-" + an + " (" + heure + ":" + minute + ":" + seconde +")*\n" + "__**" + coins.get("UBIQ").name + "**__" + "\nRank: " + coins.get("UBIQ").rank + "\nPrice : " + coins.get("UBIQ").price_usd + " :dollar: (USD)\n" + "Percent Change 1h : " + coins.get("UBIQ").percent_change_1h + " %" + pc1 + "\nPercent Change 24h : " + coins.get("UBIQ").percent_change_24h + " %" + pc2 + "\nPercent Change 7d : " + coins.get("UBIQ").percent_change_7d + " %" + pc3 );// Prints price of UBIQ in USD
+            console.log("Commande $pUBIQ effectuée");
+            
         });
-    }
-    
-    if (message.content === "$loop") { 
-      var interval = setInterval (function () {
-        message.channel.send("123")
-      }, 300000); 
-    }
-    
 });
